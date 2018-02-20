17c88a995f011e2f6a1942146775985a609b2acc
Yana <YanaLSoltan@gmail.com>
2018-02-20 17:26:49 +0300

    task 2

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@
  * @param preferences - an array of integers. Indices of people, whom they love
  * @returns number of love triangles
  */
-module.exports = function getLoveTrianglesCount(preferences = []) {
+//module.exports =
+function getLoveTrianglesCount(preferences = []) {
+
     var count = 0;
     var firstEl = 0;
     var secondEl = 0;
@@ -11,23 +13,26 @@ module.exports = function getLoveTrianglesCount(preferences = []) {
 
     for (var i = 0; i < lengthOfPreference; i++) {
 
-        loveFirst = preferences[i];
+        firstEl = preferences[i];
         if (verification(firstEl)) {
             secondEl = preferences[firstEl - 1];
             if (verification(secondEl)) {
                 thitdEl = preferences[secondEl - 1];
-                if (thitdEl - i === 1 && secondEl != i + 1) count++;
+                if (thitdEl  - 1 == i && secondEl != i + 2) count++;
             }
         }
-        return count / 3;
-    }
 
+    }
+    return count / 3;
+    
     function verification(valueElement) {
-        if ((valueElement < lengthOfPreference) || (valueElement == lengthOfPreference)) {
+        if ((valueElement < lengthOfPreference ) || (valueElement == lengthOfPreference)) {
             return true;
         } else {
             return false;
         }
 
     }
-};
+}
+console.log(getLoveTrianglesCount([2,3,1,5,6,4]));
+;
